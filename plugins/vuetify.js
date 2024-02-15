558818169172cc25b107ff5d82058b1fabf7e4bf
Ruben Decrop <ruben@chessdevil.net>
2024-02-15 18:08:01 +0100

    overruling OK

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,12 +1,8 @@
-import { createVuetify } from "vuetify";
-import * as directives from "vuetify/directives";
-import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css"
+import "vuetify/styles"
+import { createVuetify } from "vuetify"
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify({
-    ssr: true,
-    directives,
-  });
-
-  nuxtApp.vueApp.use(vuetify);
-});
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({})
+  app.vueApp.use(vuetify)
+})
